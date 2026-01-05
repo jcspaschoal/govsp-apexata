2dcefd860e9b0502f495e1631d43445ba9f6f07d
José Paschoal <54783735+jcspaschoal@users.noreply.github.com>
2026-01-05 10:25:05 +0000

    Update api.ts

--- src/service/api.ts
+++ src/service/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { store } from "@/store";
 import { logout } from "@/store/slices/authSlice";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL || "";
 
 const api = axios.create({
     baseURL: API_URL,
